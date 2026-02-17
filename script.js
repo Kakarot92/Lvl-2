eadf0697c08528012971a9e511438239b6f0e5d6
Kakarot92 <47473495+Kakarot92@users.noreply.github.com>
2026-02-17 17:16:28 +0100

    variables homework

---
+++ script.js
@@ -0,0 +1,23 @@
+let fullName = "Bogdan Stojanovic";
+let age = 33;
+let contry = "Serbia";
+let favoriteHobby = "Programming";
+let isStudent = true;
+
+console.log(
+    `Name : ${fullName}
+    Age : ${age}
+    Country : ${contry}
+    Favorite Hobby : ${favoriteHobby}
+    Is Student : ${isStudent}`
+);
+
+favoriteHobby = "Playing Basketball";
+
+console.log(
+    `Name : ${fullName}
+    Age : ${age}
+    Country : ${contry}
+    Favorite Hobby : ${favoriteHobby}
+    Is Student : ${isStudent}`
+);
