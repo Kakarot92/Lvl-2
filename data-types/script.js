d6022ce3893f2a1c7f4b548fe9a42af4d70bc176
Kakarot92 <47473495+Kakarot92@users.noreply.github.com>
2026-02-20 11:48:28 +0100

    data-types homework

---
+++ data-types/script.js
@@ -0,0 +1,16 @@
+let dayNames = "Friday";
+let stepsWalked = 5000;
+let waterCups = 8;
+let didWorkout = false;
+let eveningPlans = null;
+let favoriteSnack = undefined;
+
+console.log("Day: " + dayNames, "Steps Walked: " + stepsWalked,
+    "Cups of Water: " + waterCups, "Did Workout: " + didWorkout, 
+    "Evening Plans: " + eveningPlans, "Favorite Snack: " + favoriteSnack);
+
+favoriteSnack = "Chocolate";
+
+console.log("Day: " + dayNames, "Steps Walked: " + stepsWalked,
+    "Cups of Water: " + waterCups, "Did Workout: " + didWorkout, 
+    "Evening Plans: " + eveningPlans, "Favorite Snack: " + favoriteSnack);
