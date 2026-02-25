ddb4653b0b825c9e030b390d88adcf879b56ea91
Kakarot92 <47473495+Kakarot92@users.noreply.github.com>
2026-02-25 17:30:13 +0100

    operators homework

---
+++ operators/script.js
@@ -0,0 +1,11 @@
+let monthlyIncome = 5000;
+let rentCost = 1500;
+let foodCost = 800;
+let transportCost = 300;
+let funBudget = 400;
+
+let totalExpenses = rentCost + foodCost + transportCost + funBudget;
+let moneyLeft = monthlyIncome - totalExpenses;
+
+console.log("Total Expenses: $" + totalExpenses);
+console.log("Money Left: $" + moneyLeft);
